d25dc1690824591988f5653ad6b6169b81456651
Daria <prihachda@gmail.com>
2025-03-02 20:34:35 +0300

    tests?

--- rest-api/user_service/app.py
+++ rest-api/user_service/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from datetime import datetime
